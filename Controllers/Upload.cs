d641a14ed1f4fc70a4ea4dec964f0ba37b9bc8b2
Tri <tri97nguyen@gmail.com>
2020-10-25 04:00:05 +0000

    /upload/rubric can upload and read file line by line. Todo: refactor IFromFileExtention.ReadAsList() out of Controller, into a service

--- Controllers/Upload.cs
+++ Controllers/Upload.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using parser.Models;
 
@@ -20,8 +24,22 @@ namespace parser.Controllers
         [HttpPost]
         public IActionResult Rubric(UploadRubricIdData rubricIdData)
         {
+            rubricIdData.uploadFile.ReadAsList();
             return RedirectToAction("Success");
         }
         
+    }
+    public static class IFormFileExtention
+    {
+        public static List<string> ReadAsList(this IFormFile file)
+        {
+            var result = new List<string>();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0) result.Add(reader.ReadLine());
+            }
+            foreach (var line in result) System.Console.WriteLine(line);
+            return result;
+        }
     }
 }
